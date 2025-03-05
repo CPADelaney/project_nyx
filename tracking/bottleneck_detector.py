4b354437dabff8637c8ecf0f4bdd364b859f43b6
CPADelaney <132380794+CPADelaney@users.noreply.github.com>
2025-03-05 15:51:10 -0800

    Update bottleneck_detector.py

--- tracking/bottleneck_detector.py
+++ tracking/bottleneck_detector.py
@@ -4,6 +4,7 @@ import cProfile
 import pstats
 import os
 import sqlite3
+import sys
 from io import StringIO
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from core.secure_subprocess import run
